3b99fa790ef4535e72994220511ca50249d82a62
Thiago Mônaco <thiagomonaco98@gmail.com>
2023-11-20 04:28:20 -0300

    rename application file

--- src/main/java/com/testconfigurationpoc/JavaTestConfigurationPocApplication.java
+++ src/main/java/com/testconfigurationpoc/JavaTestConfigurationPocApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaTestConfigurationPocApplication
+public class JavaTestConfigurationPocApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaTestConfigurationPocApplication.class, args);
